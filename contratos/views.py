e6beb342a470a3bf497cd33d9bf4b7983a6bb153
Tobias de Vargas <42094615+TobiasVargas@users.noreply.github.com>
2020-07-14 13:53:05 -0300

    carregamento de files e acesso a api somente se logado

--- contratos/views.py
+++ contratos/views.py
@@ -1,14 +1,20 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Contrato, Aditivo
 from .serializers import ContratoSerializer, AditivoSerializer
 
 
-# Create your views here.
 class ContratoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Contrato.objects.all()
     serializer_class = ContratoSerializer
 
+
 class AditivoViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Aditivo.objects.all()
     serializer_class = AditivoSerializer
--- tcc/urls.py
+++ tcc/urls.py
@@ -1,7 +1,9 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from contratos import views
+from django.conf import settings
 
 router = routers.DefaultRouter()
 router.register('contratos', views.ContratoViewSet)
@@ -11,4 +13,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
